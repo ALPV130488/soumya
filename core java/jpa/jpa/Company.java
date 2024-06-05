a8b4db070068b92a86d434b1d60ca2c0531406e3
LAKSHMI A <108331858+ALPV130488@users.noreply.github.com>
2024-06-05 07:55:02 +0530

    Add files via upload

--- core java/jpa/jpa/Company.java
+++ core java/jpa/jpa/Company.java
@@ -12,5 +12,29 @@ public class Company {
 		
 		EntityManager em = factory.createEntityManager();
 		
+		em.getTransaction().begin();
+		
+		Employee emp = new Employee("Apple", 108,534534, "india");
+		
+		em.persist(emp);//insert
+		
+//		em.merge(emp); //update
+		
+		Employee emp2 = em.find(Employee.class, "Apple"); //select
+		
+		System.out.println(emp2);
+		
+		em.remove(emp2); //delete
+		
+		em.persist(emp);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		
+		System.out.println("Data is inserted successfully");
+		
+		
+		
 	}
 }
